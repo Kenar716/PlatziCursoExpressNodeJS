e7a3e8394f0f135c47824a7cf50d6aaefc7652b9
Carlos Valdez <31641682+Kenar716@users.noreply.github.com>
2020-07-12 15:45:32 -0500

    Se incorporan el uso de test para validar el funcionamiento de la webapp. Se instalan las dependencias necesarias: npm i -D supertest mocha sinon proxyquire Para implementar las pruebas de hace uso del principio de Inversion de Control (Inversion of Control - IoC). Se modifica el mocks de products para agregar mas products y ademas de agregar la funcionalidad de que regresen otras funciones, esto con el objetivo de que las pruebas no necesariamente interactuen con la base de datos. En testServer se crea un servidor de pruebas con el cual se va a interactuar durante la ejecución de las pruebas. Se modifica routes/api/products para encapsular todos los métodos dentro de la función productsApi y se modifica el index para mandar a llamar esta función. En el archivo routes.api.products.test.js se definen las pruebas que se realizan y se utiliza la función creada anteriormente de productsApi en donde se indica que se sustituya para efectos de las pruebas el servicio que interactura con la base de datos y en su lugar utilice los mocks de manera que se pueda controlar los resultados obtenidos. Las pruebas se definien en package.json con el comando 'mocha --exit' y para ejecutar las pruebas se utilizan los comando 'npm run test' o 'npm t'.

--- ecommerce/routes/api/products.js
+++ ecommerce/routes/api/products.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const passport = require('passport');
-const router = express.Router();
+
 const ProductsService = require('../../services/products');
-const productService = new ProductsService();
 
 const validation = require('../../utils/middlewares/validationHandler');
 
@@ -16,97 +15,103 @@ const {
 // JWT strategy
 require('../../utils/auth/strategies/jwt');
 
-// List
-router.get('/', async function (req, res, next) {
-    const { tags } = req.query;
-    console.log('req', req.query);
+function productsApi (app) {
+    const router = express.Router();
+    app.use('/api/products', router);
+    const productService = new ProductsService();
 
-    try {
+    // List
+    router.get('/', async function (req, res, next) {
+        const { tags } = req.query;
+        // console.log('req', req.query);
+
+        try {
         // throw new Error('This is an error.');
-        const products = await productService.getProducts({ tags });
+            const products = await productService.getProducts({ tags });
 
-        res.status(200).json({
-            data: products,
-            messsage: 'products listed'
-        });
-    } catch (err) {
-        next(err);
-    }
-});
+            res.status(200).json({
+                data: products,
+                message: 'products listed'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
 
-// Retrieve
-router.get('/:productId', async function (req, res, next) {
-    const { productId } = req.params;
-    console.log('req', req.params);
-
-    try {
-        const product = await productService.getProduct({ productId });
-
-        res.status(200).json({
-            data: product,
-            messsage: 'product retrieved'
-        });
-    } catch (err) {
-        next(err);
-    }
-});
-
-// Create
-router.post('/', validation(createProductSchema), async function (req, res, next) {
-    const { body: product } = req;
-    console.log('req', req.body);
-
-    try {
-        const createdProduct = await productService.createProduct({ product });
-
-        res.status(201).json({
-            data: createdProduct,
-            messsage: 'product created'
-        });
-    } catch (err) {
-        next(err);
-    }
-});
-
-// Replace / Create
-router.put('/:productId',
-    passport.authenticate('jwt', { session: false }),
-    validation({ productId: productIdSchema }, 'params'),
-    validation(updateProductSchema),
-    async function (req, res, next) {
+    // Retrieve
+    router.get('/:productId', async function (req, res, next) {
         const { productId } = req.params;
+        // console.log('req', req.params);
+
+        try {
+            const product = await productService.getProduct({ productId });
+
+            res.status(200).json({
+                data: product,
+                message: 'product retrieved'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    // Create
+    router.post('/', validation(createProductSchema), async function (req, res, next) {
         const { body: product } = req;
-        // console.log('req', req.params, req.body);
+        // console.log('req', req.body);
 
         try {
-            const updatedProduct = await productService.updateProduct({ productId, product });
+            const createdProduct = await productService.createProduct({ product });
 
-            res.status(200).json({
-                data: updatedProduct,
-                messsage: 'product updated'
+            res.status(201).json({
+                data: createdProduct,
+                message: 'product created'
             });
         } catch (err) {
             next(err);
         }
     });
 
-// Delete
-router.delete('/:productId',
-    passport.authenticate('jwt', { session: false }),
-    async function (req, res, next) {
-        const { productId } = req.params;
-        console.log('req', req.params);
+    // Replace / Create
+    router.put('/:productId',
+        passport.authenticate('jwt', { session: false }),
+        validation({ productId: productIdSchema }, 'params'),
+        validation(updateProductSchema),
+        async function (req, res, next) {
+            const { productId } = req.params;
+            const { body: product } = req;
+            // console.log('req', req.params, req.body);
 
-        try {
-            const deletedProduct = await productService.deleteProduct({ productId });
+            try {
+                const updatedProduct = await productService.updateProduct({ productId, product });
 
-            res.status(200).json({
-                data: deletedProduct,
-                messsage: 'product deleted'
-            });
-        } catch (err) {
-            next(err);
-        }
-    });
+                res.status(200).json({
+                    data: updatedProduct,
+                    message: 'product updated'
+                });
+            } catch (err) {
+                next(err);
+            }
+        });
 
-module.exports = router;
+    // Delete
+    router.delete('/:productId',
+        passport.authenticate('jwt', { session: false }),
+        async function (req, res, next) {
+            const { productId } = req.params;
+            // console.log('req', req.params);
+
+            try {
+                const deletedProduct = await productService.deleteProduct({ productId });
+
+                res.status(200).json({
+                    data: deletedProduct,
+                    message: 'product deleted'
+                });
+            } catch (err) {
+                next(err);
+            }
+        });
+}
+
+module.exports = productsApi;
